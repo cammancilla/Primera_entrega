27af327ad815f8f56b66579b928bc517c10aabe3
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-05-15 00:19:01 -0400

    Añadiendo validacion con JavaScript

--- js/validacionesContacto.js
+++ js/validacionesContacto.js
@@ -1,45 +1,26 @@
-// cerrar llave de jquery
-
-$(document).ready(function() {
-
-    $("btnEnviar").click(function() {
-        $("#formularioContacto").submit();
-    });
-
-
-
+const rut = document.getElementById('txtRut');
+const nombre = document.getElementById('txtNombre');
+const apellidoPaterno = document.getElementById('txtAppaterno');
+const apellidoMaterno = document.getElementById('txtApmaterno');
+const correo = document.getElementById('txtMail');
+const telefono = document.getElementById('txtTelefono');
+const comentario = document.getElementById('txtComentario');
+const parrafo = document.getElementById('warnings');
 
 
+const form = document.getElementById('#formularioContacto');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let warnings = '';
+    let regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    if (rut.value.length < 9) {
+        warnings += 'El rut no es válido <br>';
+    }
+    if (nombre.value.length < 3) {
+        warnings += 'El nombre debe ser mayor A 3 <br>';
+    }
+})
 
 
-
-    function validarDatos(rut,nombre,appaterno,appmaterno,correo,telefono,comentario)
-           if (String(rut).length == 0 || String(rut).length > 10) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El rut no puede estar vacío o tener menos de 10 caracteres</div>");
-            
-           } else if (String(nombre).length == 0 || String(nombre).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El nombre no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else if (String(appaterno).length == 0 || String(appaterno).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El apellido paterno no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else if (String(appmaterno).length == 0 || String(appmaterno).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El apellido materno no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else if (String(correo).length == 0 || String(correo).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El correo no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else if (String(telefono).length == 0 || String(telefono).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El teléfono no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else if (String(comentario).length == 0 || String(comentario).length > 50) {
-            ("check").html("div class= 'alert alert-danger w-50 mx-auto text-center'>El comentario no puede estar vacío o tener más de 50 caracteres</div>");
-            
-           } else {
-            ("check").html("div class= 'alert alert-success w-50 mx-auto text-center'>Datos enviados correctamente</div>");
-           }
-
-
-
-
-});
