42d552672add65af30ddf2ef2ae21ad421f874a0
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-05-17 23:51:40 -0400

    Formulario Contacto terminado

--- js/validacionesContacto.js
+++ js/validacionesContacto.js
@@ -1,15 +1,49 @@
-const rut = document.querySelector('[name="txtRut"]');
-const nombre = document.querySelector('[name="txtNombre"]');
-const apellidoPaterno = document.querySelector('[name="txtAppaterno"]');
-const apellidoMaterno = document.querySelector('[name="txtApmaterno"]');
-const email = document.querySelector('[name="txtMail"]');
-const telefono = document.querySelector('[name="txtTelefono"]');
-const mensaje = document.querySelector('[name="txtComentario"]');
+document.addEventListener("DOMContentLoaded", function () {
+    const rut = document.querySelector('[name="txtRut"]');
+    const nombre = document.querySelector('[name="txtNombre"]');
+    const apellidoPaterno = document.querySelector('[name="txtApellido"]');
+    const email = document.querySelector('[name="txtMail"]');
+    const telefono = document.querySelector('[name="txtTelefono"]');
+    const mensaje = document.querySelector('[name="txtComentario"]');
+    const validateEmptyField = (e) => {
+        const field = e.target;
+        const fieldValue = e.target.value;
+        if (fieldValue.trim().length <= 0) {
+            field.classList.add("invalid");
+            field.nextElementSibling.classList.add("error");
+            field.nextElementSibling.innerText = "Este campo es obligatorio";
 
+        } else {
+            field.classList.remove("invalid");
+            field.nextElementSibling.classList.remove("error");
+            field.nextElementSibling.innerText = "";
+        }
+    };
 
-rut.addEventListener("blur", function (e) {
-    const fieldValue = e.target.value;
-    if (fieldValue.value <= 0) {
-        console.log("Rut no puede estar vacio");
-    }
+    const validateEmail = (e) => {
+        const field = e.target;
+        const fieldValue = e.target.value;
+        const regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+        if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+            field.classList.add("invalid");
+            field.nextElementSibling.classList.add("error");
+            field.nextElementSibling.innerText = "Introduzca un correo valido";
+
+        } else {
+            field.classList.remove("invalid");
+            field.nextElementSibling.classList.remove("error");
+            field.nextElementSibling.innerText = "";
+        }
+    };
+
+    rut.addEventListener("blur", validateEmptyField);
+    nombre.addEventListener("blur", validateEmptyField);
+    apellidoPaterno.addEventListener("blur", validateEmptyField);
+
+    email.addEventListener("blur", validateEmptyField);
+    email.addEventListener("input", validateEmail);
+    telefono.addEventListener("blur", validateEmptyField);
+    mensaje.addEventListener("blur", validateEmptyField);
+
 });
