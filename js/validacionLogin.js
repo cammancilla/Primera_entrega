b4307c0cfb841496b5e2ca9ca01469298e1cb4f7
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-05-22 02:19:48 -0400

    Verificacion de login COMPLETO!!!

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -1,23 +1,37 @@
-// Cargar DOM
 document.addEventListener("DOMContentLoaded", function () {
+    const correo = document.getElementById('correo');
+    const password = document.getElementById('passwordLogin');
+    const button = document.getElementById("btnLogin");
+    const mensaje = document.getElementById("warnings");
 
-    const correo = document.getElementById('[name="correo"]');
-    const clave = document.getElementById('[name="passwordLogin"]');
-    const warnings = document.getElementById('errores');
-
-    const formularioLogin = document.getElementById('loginForm');
-
-    formularioLogin.addEventListener('submit', function (e) {
+    button.addEventListener("click", (e) => {
         e.preventDefault();
-        let warnings = '';
-        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-        // Validar correo
-        if (!regexEmail.test(correo.value)) {
-            document.getElementById('errores').innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Correo no debe estar vacio</div>";
-            correo.style.border = '1px solid red';
-            console.log("Deberia aparecer un error")
+        let entrar = false;
+        // Regex Correo
+        const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        // Regex Password (8 caracteres, 1 mayuscula, 1 minuscula, 1 numero, 1 caracter especial)
+        const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+        if (!regexCorreo.test(correo.value)) {
+            mensaje.innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Correo no valido</div>";
+            mensaje.classList.add("alert-danger");
+            console.log("Correo no valido");
+            entrar = true;
+            return;
         }
-    });
 
+        if (!regexPassword.test(password.value)) {
+            mensaje.innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Contraseña debe tener al menos 8 carácteres, una minuscula, 1 mayuscula, 1 numero y 1 caracter especial</div>";
+            mensaje.classList.add("alert-danger");
+            console.log("Contraseña no valida");
+            entrar = true;
+            return;
+        }
+
+        if (!entrar) {
+            mensaje.innerHTML = "<div class='alert alert-success w-50 mx-auto text-center'>Datos validados correctamente</div>";
+            mensaje.classList.add("alert-success");
+            console.log("Datos validados correctamente");
+        }
+
+    });
 });
